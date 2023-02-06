f9e72475dcb1e903e0acf41ffe821380df7fd669
Christopher Fuchs <fuchs-christopher@gmx.de>
2023-02-06 21:30:33 +0100

    worked on contact

--- js/contacts.js
+++ js/contacts.js
@@ -8,29 +8,33 @@ let contacts = [
     {
         'email': 'alex.luchs@web.de',
         'passwort': 'test432',
-        'name': 'Alex Luchs'
+        'name': 'Alex Luchs',
+        'phone': '34445'
     },
     {
         'email': 'peter.vogel@web.de',
         'passwort': 'test432',
-        'name': 'Peter Vogel'
+        'name': 'Peter Vogel',
+        'phone': '34445'
     },
     {
         'email': 'karin.berg@web.de',
         'passwort': 'test432',
-        'name': 'Karin Berg'
+        'name': 'Karin Berg',
+        'phone': '34445'
     },
     {
         'email': 'marina.bohle@web.de',
         'passwort': 'test432',
-        'name': 'Marina Bohle'
+        'name': 'Marina Bohle',
+        'phone': '34445'
     }
 ];
-
+let letters = [];
 
 
 /**
- * Creating contancts
+ * Creating contanctlist
  */
 function showContacts() {
     sortContacts(contacts);
@@ -39,11 +43,58 @@ function showContacts() {
     contactContainer.innerHTML = '';
     for (let i = 0; i < contacts.length; i++) {
         let contact = contacts[i];
+        let color = randomColor();
+        let contactName = contacts[i]['name'];
+        let bothFirstLetter = splitName(contactName);
         contactContainer.innerHTML += `
-        <span></span>
+        <div class="over-div-letter-name-email">
+         <div style="background-color: ${color}" class="letter-circle">${bothFirstLetter}</div>
+          <div>
+            <div class="single-name">${contact.name}</div>
+            <span class="contact-email">${contact.email}</span>
+           </div>
+        </div>
+        `;
+    }
+    creatSingleLetters();
+}
+
+
+function creatSingleLetters() {
+    filterFirstLetter();
+    renderBigLetter();
+}
+
+
+function splitName(fullName) {
+    let namePara = fullName.split(" ");
+    let firstName = namePara[0];
+    let lastName = namePara[namePara.length - 1];
+    let bothFirstLetter = firstName.charAt(0) + lastName.charAt(0);
+    return bothFirstLetter
+}
+
+
+function filterFirstLetter() {
+    for (let i = 0; i < contacts.length; i++) {
+        let contact = contacts[i];
+        let firstLetter = contact['name'].charAt(0).toLocaleLowerCase();
+        if (!letters.includes(firstLetter)) {
+            letters.push(firstLetter);
+        }
+    }
+}
+
+
+function renderBigLetter() {
+    let bigLetter = document.getElementById('first-big-letter');
+    bigLetter.innerHTML = '';
+    for (let i = 0; i < letters.length; i++) {
+        let letter = letters[i];
+        let letterBig = letter.charAt(0).toUpperCase();
+        bigLetter.innerHTML += `
+        <span>${letterBig}</span>
         <div class="border-for-contacts"></div>
-        <div class="single-name">${contact.name}</div>
-        <span class="contact-email">${contact.email}</span>
         `;
     }
 }
