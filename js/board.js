4c5df6989188a134342e8cec664927998e3e0f66
hasancskn38 <95541333+hasancskn38@users.noreply.github.com>
2023-02-06 20:14:41 +0100

    worked on board

--- js/board.js
+++ js/board.js
@@ -15,11 +15,54 @@ async function includeHTML() {
 
 // openAddTaskPopUp Function
 function openAddTaskPopUp() {
-    document.getElementById('popup').classList.remove('d-none');
     document.getElementById('overlay').classList.remove('d-none');
+    document.getElementById('popup').classList.remove('hide');
+    document.getElementById('popup').classList.add('show');
+    document.getElementById('popup').classList.remove('d-none');
+
 }
 
 function closeAddTaskPopUp() {
-    document.getElementById('popup').classList.add('d-none');
     document.getElementById('overlay').classList.add('d-none');
+    document.getElementById('popup').classList.add('hide');
+    document.getElementById('popup').classList.remove('show');
+}
+
+let priorityImg = document.getElementById('img')
+let urgent = document.getElementById('urgent')
+let medium = document.getElementById('medium')
+let low = document.getElementById('low')
+// Change the Color of the different Priority Levels
+function changeUrgentColor() {
+    if(urgent.classList.contains('urgent')) {
+        urgent.classList.remove('urgent') 
+        
+    } else {
+        urgent.classList.add('urgent')
+        low.classList.remove('low')
+        medium.classList.remove('medium')
+    }
+}
+
+function changeMediumColor() {
+    if(medium.classList.contains('medium')) {
+        medium.classList.remove('medium') 
+        
+    } else {
+        medium.classList.add('medium')
+        urgent.classList.remove('urgent')
+        low.classList.remove('low')
+    }
+}
+
+function changeLowColor() {
+    if(low.classList.contains('low')) {
+        low.classList.remove('low')
+        
+    } else {
+        low.classList.add('low')
+        urgent.classList.remove('urgent')
+        medium.classList.remove('medium')
+    }
+    
 }
