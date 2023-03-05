7f9bc8770a6ab08ba54ee6c26a7ec3bd912ac2a2
MarcelDragZ <marcelhe98@gmx.de>
2023-03-05 21:13:28 +0100

    gdf
    fdg

--- js/summary.js
+++ js/summary.js
@@ -2,9 +2,12 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 =======
 =======
+=======
+>>>>>>> parent of ef59c3e (asd)
 let testData = [
     {
         title: 'Finish UI',
@@ -16,6 +19,55 @@ let testData = [
         assignedContacts: ['HC', 'CD'],
         id: '0'
     },
+<<<<<<< HEAD
+=======
+
+    {
+        title: 'Finish Backend',
+        cat: 'Marketing',
+        description: 'This is a Task Description',
+        status: 'inprogress',
+        date: '05-08-2022',
+        priority: 'Urgent',
+        assignedContacts: ['HC', 'CD', 'CD'],
+        id: '1'
+    },
+
+    {
+        title: 'Look for Social Media Marketing Agency',
+        cat: 'Backoffice',
+        description: 'This is a Task Description',
+        status: 'feedback',
+        date: '05-08-2022',
+        priority: 'Urgent',
+        assignedContacts: ['CD', 'CD'],
+        id: '2'
+    },
+
+    {
+        title: 'Call with Steven',
+        cat: 'Sales',
+        description: 'This is a Task Description',
+        status: 'done',
+        date: '05-08-2022',
+        priority: 'Medium',
+        assignedContacts: ['HC', 'CD'],
+        id: '3'
+    },
+
+    {
+        title: 'Call with Test',
+        cat: 'Sales',
+        description: 'This is a Task Description',
+        status: 'done',
+        date: '05-08-2022',
+        priority: 'Low',
+        assignedContacts: ['CF'],
+        id: '4'
+    },
+];
+
+>>>>>>> parent of ef59c3e (asd)
 
     {
         title: 'Finish Backend',
