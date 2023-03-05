a7296967f3386fdce36745e3f38a900c87675e17
MarcelDragZ <marcelhe98@gmx.de>
2023-03-05 21:08:41 +0100

    ads

--- js/summary.js
+++ js/summary.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 80d99da (worked on summary)
 // Generate Greeting Based on real world time
 // setInterval(function() {
 //     let date = new Date();
@@ -23,7 +19,7 @@ async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); 
+        file = element.getAttribute("w3-include-html");
         let resp = await fetch(file);
         if (resp.ok) {
             element.innerHTML = await resp.text();
@@ -33,3 +29,81 @@ async function includeHTML() {
     }
 }
 
+
+renderTasks();
+
+
+function renderTasks() {
+    let boardNum = document.getElementById('onBoardNum');
+    boardNum.innerHTML = countTasksOnBoard();
+    let numProgress = document.getElementById('numProgress');
+    numProgress.innerHTML = countTasksInProcess();
+    let awaitingFeedbackNum = document.getElementById('awaitingFeedbackNum');
+    awaitingFeedbackNum.innerHTML = countAwaitingFeedback();
+    let numDone = document.getElementById('numDone');
+    numDone.innerHTML = countTasksInDone();
+    let numToDo = document.getElementById('numToDo');
+    numToDo.innerHTML = countTasksToDo();
+    let numUrgent = document.getElementById('numUrgent');
+    numUrgent.innerHTML = countUrgent();
+}
+
+
+function countTasksOnBoard() {
+    let onBoard = testData.length;
+    return onBoard;
+}
+
+
+function countTasksInProcess() {
+    let inProgress = 0;
+    for (let i = 0; i < testData.length; i++) {
+        if (testData[i]['status'] == 'inprogress') {
+            inProgress++;
+        }
+    }
+    return inProgress;
+}
+
+
+function countAwaitingFeedback() {
+    let inAwaitingFeedback = 0;
+    for (let i = 0; i < testData.length; i++) {
+        if (testData[i]['status'] == 'feedback') {
+            inAwaitingFeedback++;
+        }
+    }
+    return inAwaitingFeedback;
+}
+
+
+function countTasksInDone() {
+    let inDone = 0;
+    for (let i = 0; i < testData.length; i++) {
+        if (testData[i]['status'] == 'done') {
+            inDone++;
+        }
+    }
+    return inDone;
+}
+
+
+function countTasksToDo() {
+    let inToDo = 0;
+    for (let i = 0; i < testData.length; i++) {
+        if (testData[i]['status'] == 'todo') {
+            inToDo++;
+        }
+    }
+    return inToDo;
+}
+
+
+function countUrgent() {
+    let inUrgent = 0;
+    for (let i = 0; i < testData.length; i++) {
+        if ( testData[i]['priority'] == 'Urgent')
+        inUrgent++;
+    }
+    return inUrgent;
+}
