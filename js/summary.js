7c201d21ad9555f97ebedfe14cc8baa414ccf5e2
Christopher Fuchs <fuchs-christopher@gmx.de>
2023-03-05 19:38:12 +0100

    worked on summary

--- js/summary.js
+++ js/summary.js
@@ -1,4 +1,59 @@
+let testData = [
+    {
+        title: 'Finish UI',
+        cat: 'Design',
+        description: 'This is a Task Description',
+        status: 'todo',
+        date: '05-08-2022',
+        priority: 'Urgent',
+        assignedContacts: ['HC', 'CD'],
+        id: '0'
+    },
 
+    {
+        title: 'Finish Backend',
+        cat: 'Marketing',
+        description: 'This is a Task Description',
+        status: 'inprogress',
+        date: '05-08-2022',
+        priority: 'Urgent',
+        assignedContacts: ['HC', 'CD', 'CD'],
+        id: '1'
+    },
+
+    {
+        title: 'Look for Social Media Marketing Agency',
+        cat: 'Backoffice',
+        description: 'This is a Task Description',
+        status: 'feedback',
+        date: '05-08-2022',
+        priority: 'Urgent',
+        assignedContacts: ['CD', 'CD'],
+        id: '2'
+    },
+
+    {
+        title: 'Call with Steven',
+        cat: 'Sales',
+        description: 'This is a Task Description',
+        status: 'done',
+        date: '05-08-2022',
+        priority: 'Medium',
+        assignedContacts: ['HC', 'CD'],
+        id: '3'
+    },
+
+    {
+        title: 'Call with Test',
+        cat: 'Sales',
+        description: 'This is a Task Description',
+        status: 'done',
+        date: '05-08-2022',
+        priority: 'Low',
+        assignedContacts: ['CF'],
+        id: '4'
+    },
+];
 
 
 // Generate Greeting Based on real world time
